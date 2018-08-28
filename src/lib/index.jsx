4f235abff9f18a6364a810bd649e35de7811768e
Joshua Richey <j@joshuar.com>
2018-08-27 22:13:45 -0700

    Add support for custom RVH units on arbitrary styles.

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
 
+const rvhRegex = /(\d+(\.\d*)?)rvh\s*$/;
+
 class Div100vh extends React.Component {
   constructor() {
     super();
     this.myRef = React.createRef();
-    this.setHeightAsWindow = this.setHeightAsWindow.bind(this);
+    this.computeRvhStyles = this.computeRvhStyles.bind(this);
   }
 
-  setHeightAsWindow() {
+  // On window resize, recalculate any rvh unit style properties
+  computeRvhStyles() {
     const node = this.myRef.current;
-    node.style.height = window.innerHeight + 'px';
+    let rvhPropertyFound = false;
+    Object.entries(this.props.style).forEach(([property, rawValue]) => {
+      const match = rvhRegex.exec(rawValue);
+      if (match != null) {
+        rvhPropertyFound = true;
+        // Guarantee that this only runs for numbers
+        const extractedValue = parseFloat(match[0]);
+        node.style[property] = extractedValue / 100 * window.innerHeight + 'px';
+      }
+    });
+    // Default to height 100vh if no rvh found in style
+    if (!rvhPropertyFound) {
+      node.style.height = window.innerHeight + 'px';
+    }
   }
 
   componentDidMount() {
-    this.setHeightAsWindow();
-    window.addEventListener('resize', this.setHeightAsWindow);
+    this.computeRvhStyles();
+    window.addEventListener('resize', this.computeRvhStyles);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.setHeightAsWindow);
+    window.removeEventListener('resize', this.computeRvhStyles);
   }
 
   render() {
