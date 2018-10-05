81a00d741570536c9d119df5ba241e7b09e5fa75
Jeff Auriemma <jdauriemma@gmail.com>
2018-10-05 12:15:21 -0400

    Remove short-circuit
    The docs specify `Div100vh` as a component that can be used as a drop-in
    replacement for <div style={{ height: '100vh' }} />.
    
    Current behavior does not match that - if there is no declared `style`
    prop, the resizing behavior does not execute and the component does not
    work as expected.
    
    This patch allows for the innerHeight fix to run even if no style prop
    is present.

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -3,32 +3,49 @@ import React from 'react';
 const rvhRegex = /(\d+(\.\d*)?)rvh\s*$/;
 
 class Div100vh extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.myRef = React.createRef ?
       React.createRef() :
       null;
     this.computeRvhStyles = this.computeRvhStyles.bind(this);
   }
 
+  getRef() {
+    return this.myRef ?
+      (this.myRef.current || this.myRef) :
+      null;
+  }
+
+  setNodeHeightToWindowInnerHeight() {
+    const node = this.getRef();
+    return node ?
+      node.style.height = window.innerHeight + 'px' :
+      null;
+  }
+
   // On window resize, recalculate any rvh unit style properties
   computeRvhStyles() {
-    if (!this.props.style) return;
+    if (!this.props.style) {
+      return this.setNodeHeightToWindowInnerHeight();
+    }
 
-    const node = this.myRef.current;
     let rvhPropertyFound = false;
+    const node = this.getRef();
+
     Object.entries(this.props.style).forEach(([property, rawValue]) => {
       const match = rvhRegex.exec(rawValue);
-      if (match != null) {
+      if (node && match != null) {
         rvhPropertyFound = true;
         // Guarantee that this only runs for numbers
         const extractedValue = parseFloat(match[0]);
         node.style[property] = extractedValue / 100 * window.innerHeight + 'px';
       }
     });
+
     // Default to height 100vh if no rvh found in style
     if (!rvhPropertyFound) {
-      node.style.height = window.innerHeight + 'px';
+      this.setNodeHeightToWindowInnerHeight();
     }
   }
 
