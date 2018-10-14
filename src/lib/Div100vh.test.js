b681b1a4ac2f877d14236eb74298b5cac168a9e2
Mikhail Vasin <michaelvasin@gmail.com>
2018-10-14 18:43:08 +0500

    Flatten tests

--- src/lib/Div100vh.test.js
+++ src/lib/Div100vh.test.js
@@ -1,65 +1,70 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import Div100vh, { parseStylesAndConvertRvhToPx } from './index.jsx'
+import Div100vh from './Div100vh';
 
-const renderComponent = props => renderer.create(
-  <Div100vh {...props} />,
-);
+// I wonder why this doesn't work?
+// const getWindowHeight = jest.fn();
+// getWindowHeight.mockReturnValue(1000);
+// so I have to use this instead
+window.innerHeight = 1000;
 
+const renderComponent = props => renderer.create(<Div100vh {...props} />);
 const getDivProps = component => component.root.findByType('div').props;
 
-describe('Rendering and unmounting in the DOM', () => {
-  it('should not crash', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Div100vh />, div);
-    ReactDOM.unmountComponentAtNode(div);
+it("uses {height: '100rvh'} as a default if style prop is undefined", () => {
+  const component = renderComponent({});
+  const props = getDivProps(component);
+  expect(props).toEqual({ style: { height: '1000px' } });
+});
+
+describe('When style prop is specified and contains no rvh', () => {
+  it('does not modify an empty style object', () => {
+    const component = renderComponent({ style: {} });
+    const props = getDivProps(component);
+    expect(props).toEqual({ style: {} });
+  });
+  it('does not modify a style with height in px', () => {
+    const component = renderComponent({ style: { height: '123px' } });
+    const props = getDivProps(component);
+    expect(props).toEqual({ style: { height: '123px' } });
+  });
+  it('passes through all style properties unchanged', () => {
+    const component = renderComponent({ style: { color: 'green' } });
+    const props = getDivProps(component);
+    expect(props).toEqual({ style: { color: 'green' } });
   });
 });
 
-describe('When component mounts and/or resizes', () => {
-  describe('when no props are passed to the component', () => {
-    it('should render a <div> with a height value that matches the visible area of the viewport when there are no props passed', () => {
-      window.innerHeight = 1;
-      const component = renderComponent({});
-      const props = getDivProps(component);
-      expect(props).toEqual({ style: { height: '1px' } });
-    });
+describe('rvh unit conversion', () => {
+  it('converts a simple rvh value', () => {
+    const component = renderComponent({ style: { height: '50.5rvh' } });
+    const props = getDivProps(component);
+    expect(props).toEqual({ style: { height: '505px' } });
   });
-
-  describe('when props are passed to the component', () => {
-    describe('when props do not contain a style object', () => {
-      it('should render a <div> with all the declared props plus a style.height value that matches the visible area of the viewport', () => {
-        window.innerHeight = 1;
-        const component = renderComponent({ foo: 'foo', bar: 'bar' });
-        const props = getDivProps(component);
-        expect(props).toEqual({ foo: 'foo', bar: 'bar', style: { height: '1px' } });
-      });
+  it('converts a shorthand value with multiple rvh entries', () => {
+    const component = renderComponent({
+      style: { margin: '1rvh 2px 1.5rvh 3%' }
     });
-
-    describe('when props contain a style object', () => {
-      describe('when the style object contains no values that use the rvh unit', () => {
-        it('should render a <div> with all the declared props plus a style.height value that matches the visible area of the viewport', () => {
-          window.innerHeight = 1;
-          const component = renderComponent({ foo: 'foo', bar: 'bar', style: { maxHeight: '100%' } });
-          const props = getDivProps(component);
-          expect(props).toEqual({ foo: 'foo', bar: 'bar', style: { height: '1px', maxHeight: '100%' } });
-        });
-      });
-
-      describe('when the style object contains values that use the rvh unit', () => {
-        it('should render a <div> with all the declared props, and parse the props so that all rvh values are proportionally adjusted to the visible area of the viewport', () => {
-          window.innerHeight = 1000;
-          const component = renderComponent({ foo: 'foo', bar: 'bar', style: { maxHeight: '99rvh' } });
-          const props = getDivProps(component);
-          expect(props).toEqual({ foo: 'foo', bar: 'bar', style: { maxHeight: '990px' } });
-        });
-      });
+    const props = getDivProps(component);
+    expect(props).toEqual({ style: { margin: '10px 2px 15px 3%' } });
+  });
+  it('converts an rvh value and passes through other style props', () => {
+    const component = renderComponent({
+      style: { fontSize: '5rvh', color: 'red' }
     });
+    const props = getDivProps(component);
+    expect(props).toEqual({ style: { fontSize: '50px', color: 'red' } });
   });
 });
 
-xdescribe('parseStylesAndConvertRvhToPx', () => {
-  test('pending', () => {
-  });
+it('passes through to the underlying div the other props with rvh', () => {
+  const component = renderComponent({ foo: 'bar', style: { padding: '1rvh' } });
+  const props = getDivProps(component);
+  expect(props).toEqual({ foo: 'bar', style: { padding: '10px' } });
+});
+
+it('passes through to the underlying div the other props without rvh', () => {
+  const component = renderComponent({ foo: 'bar', style: { color: 'red' } });
+  const props = getDivProps(component);
+  expect(props).toEqual({ foo: 'bar', style: { color: 'red' } });
 });
