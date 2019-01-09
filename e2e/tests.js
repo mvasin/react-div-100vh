9d000bcec84e784c9df62a391bd68ae253d0d599
Mikhail Vasin <mvasin@users.noreply.github.com>
2019-01-09 22:23:15 +0300

    Fix indentation

--- e2e/tests.js
+++ e2e/tests.js
@@ -2,7 +2,7 @@ const server = require('./server');
 const {By} = require('selenium-webdriver');
 
 exports.h1IsPresent = async function h1IsPresent(driver) {
-    const url = await server.getURL();
-    await driver.get(url);
-    await driver.findElement(By.css('h1'));
+  const url = await server.getURL();
+  await driver.get(url);
+  await driver.findElement(By.css('h1'));
 };
