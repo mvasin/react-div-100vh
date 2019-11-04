79305654c5156aa7ef3d5d0ca2e9f7014cade276
Roman Kaspar <roman@salsitasoft.com>
2019-11-05 00:02:45 +0100

    use document.documentElement.clientHeight instead of window.innerHeight

--- src/lib/getWindowHeight.js
+++ src/lib/getWindowHeight.js
@@ -1,6 +1,6 @@
 // extracted into a separate module so it's easier to mock with Jest
 function getWindowHeight() {
-  return window.innerHeight;
+  return (document && document.documentElement && document.documentElement.clientHeight) || window.innerHeight;
 }
 
 export default getWindowHeight;
