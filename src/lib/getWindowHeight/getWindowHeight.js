41f3cb3c1e53757e9fefd19dd105ed3788b4d08c
Mikhail Vasin <12434833+mvasin@users.noreply.github.com>
2019-12-13 07:14:41 +0100

    Revert 0.3.6 and 0.3.7 (#29)

--- src/lib/getWindowHeight/getWindowHeight.js
+++
@@ -1,9 +0,0 @@
-function getWindowHeight() {
-  if (typeof document === 'undefined' && typeof window === 'undefined') {
-    return 0;
-  }
-
-  return (document && document.documentElement && document.documentElement.clientHeight) || window.innerHeight;
-}
-
-export default getWindowHeight;
