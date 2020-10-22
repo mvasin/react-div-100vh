32fb4c1888a071fc5225bd33dd7ba02cb15f18c9
Mikhail Vasin <12434833+mvasin@users.noreply.github.com>
2020-10-22 12:51:56 +0200

    --wip-- [skip ci]

---
+++ packages/test-app/App.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export const App: React.FC = () => <div>hi</div>
+
--- packages/test-app/index.tsx
+++ packages/test-app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import { Demo } from './demo'
+import { App } from './App'
 
-render(<Demo />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
